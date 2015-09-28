71e6eaaa617e37aee9609e49783fbd110b3e834e
FateSolo <FateSolo@mail.bistu.edu.cn>
2015-09-28 23:22:06 +0800

    第一次彻底重构(未测试)

--- Database.py
+++ Database.py
@@ -3,34 +3,40 @@ __author__ = 'fate'
 import MySQLdb
 
 
-def query(username):
+def query(username, nickname):
     conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="FiveChess", charset="utf8")
     cursor = conn.cursor()
-    sqlStr = "select * from User where UserName='" + username + "'"
-    cursor.execute(sqlStr)
+
+    sqlStr = "select * from User where 1 = 1"
+    args = []
+
+    if username is not None:
+        sqlStr += " and UserName = %s"
+        args.append(username)
+
+    if nickname is not None:
+        sqlStr += " and NickName = %s"
+        args.append(nickname)
+
+    cursor.execute(sqlStr, args)
     data = cursor.fetchall()
+
     cursor.close()
     conn.close()
-    return data
 
-
-def query2(nickname):
-    conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="FiveChess", charset="utf8")
-    cursor = conn.cursor()
-    sqlStr = "select * from User where NickName='" + nickname + "'"
-    cursor.execute(sqlStr)
-    data = cursor.fetchall()
-    cursor.close()
-    conn.close()
     return data
 
 
 def insert(username, password, nickname):
     conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="FiveChess", charset="utf8")
     cursor = conn.cursor()
-    sqlStr = "insert into User values('" + username + "','" + password + "','" + nickname + "',0,0,0)"
-    cursor.execute(sqlStr)
+
+    sqlStr = "insert into User values (%s, %s, %s, 0, 0, 0)"
+    args = [username, password, nickname]
+
+    cursor.execute(sqlStr, args)
     conn.commit()
+
     cursor.close()
     conn.close()
 
@@ -38,9 +44,13 @@ def insert(username, password, nickname):
 def delete(username):
     conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="FiveChess", charset="utf8")
     cursor = conn.cursor()
-    sqlStr = "delete from User where UserName='" + username + "'"
-    cursor.execute(sqlStr)
+
+    sqlStr = "delete from User where UserName= %s"
+    args = [username]
+
+    cursor.execute(sqlStr, args)
     conn.commit()
+
     cursor.close()
     conn.close()
 
@@ -48,11 +58,39 @@ def delete(username):
 def update(username, password, nickname, win, lose, draw):
     conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="FiveChess", charset="utf8")
     cursor = conn.cursor()
-    sqlStr = "update User set PassWord = '" + password + "', NickName = '" + nickname + "', Win = " + str(
-        win) + ", Lose = " + str(lose) + ", Draw = " + str(draw) + " where UserName = '" + username + "'"
-    cursor.execute(sqlStr)
-    conn.commit()
+
+    sqlStr = "update User set"
+    args = []
+
+    tmp = ""
+
+    if password is not None:
+        tmp += " PassWord = %s,"
+        args.append(password)
+
+    if nickname is not None:
+        tmp += " NickName = %s,"
+        args.append(nickname)
+
+    if win is not None:
+        tmp += " Win = %s,"
+        args.append(win)
+
+    if lose is not None:
+        tmp += " Lose = %s,"
+        args.append(lose)
+
+    if draw is not None:
+        tmp += " Draw = %s,"
+        args.append(draw)
+
+    if tmp != "":
+        tmp = tmp[:-1]
+        tmp += " where UserName = %s"
+        args.append(username)
+
+        cursor.execute(sqlStr, args)
+        conn.commit()
+
     cursor.close()
     conn.close()
-
-
--- FiveChessServer.py
+++ FiveChessServer.py
@@ -17,205 +17,274 @@ chessBoard = c_ubyte * 225
 
 class ChessServer(Protocol):
     def dataReceived(self, data):
-        msgType, msg = data.split(' ', 1)
-        if msgType == '/Login':
-            username, password = msg.split(' ', 1)
-            user = query(username)
-            if user == ():
-                data = '/UsernameNotExist'
-            elif user[0][1] != password:
-                data = '/PasswordError'
-            elif user[0] in self.factory.player:
-                data = '/UserHasLogged'
-            else:
-                data = str(user[0][0]) + ' ' + str(user[0][1]) + ' ' + str(user[0][2]) + ' ' + str(
-                    user[0][3]) + ' ' + str(user[0][4]) + ' ' + str(user[0][5])
-                self.factory.player.append(user[0])
-                self.factory.status.append("/idle")
-                self.factory.chessBoard.append(chessBoard())
-                userList = "/UpdateUserList "
-                for i in self.factory.player:
-                    userList += str(i[2]) + ' '
-                userList = userList[:-1]
-                for client in self.factory.client:
-                    client.transport.write(userList)
-                self.factory.client.append(self)
+        msg_type, msg = data.split(' ', 1)
 
-        elif msgType == '/Register':
-            username, password = msg.split(' ')
-            user = query(username)
-            if user:
-                data = '/UsernameExist'
-            else:
-                insert(username, password, username)
-                user = query(username)
-                data = str(user[0][0]) + ' ' + str(user[0][1]) + ' ' + str(user[0][2]) + ' ' + str(
-                    user[0][3]) + ' ' + str(user[0][4]) + ' ' + str(user[0][5])
-                self.factory.player.append(user[0])
-                self.factory.status.append("/idle")
-                self.factory.chessBoard.append(chessBoard())
-                userList = "/UpdateUserList "
-                for i in self.factory.player:
-                    userList += str(i[2]) + ' '
-                userList = userList[:-1]
-                for client in self.factory.client:
-                    client.transport.write(userList)
-                self.factory.client.append(self)
-
-        elif msgType == '/GetUserList':
-            data = "/UpdateUserList "
-            for i in self.factory.player:
-                data += str(i[2]) + ' '
-            data = data[:-1]
-
-        elif msgType == '/GetUserInfo':
-            user = query(msg)
-            data = "/UserInfo " + str(user[0][2]) + ' ' + str(user[0][3]) + ' ' + str(user[0][4]) + ' ' + str(
-                user[0][5])
-
-        elif msgType == '/SendMsg':
-            for client in self.factory.client:
-                client.transport.write(data)
-            return
-
-        elif msgType == '/AlterName':
-            user = query2(msg)
-            if user:
-                data = '/AlterNameError'
-            else:
-                username, password, data, win, lose, draw = self.factory.player[self.factory.client.index(self)]
-                update(username, password, msg, win, lose, draw)
-                self.factory.player[self.factory.client.index(self)] = (username, password, msg, win, lose, draw)
-                data = '/AlterNameSuccess ' + msg
-                userList = "/UpdateUserList "
-                for i in self.factory.player:
-                    userList += str(i[2]) + ' '
-                userList = userList[:-1]
-                for client in self.factory.client:
-                    if client != self:
-                        client.transport.write(userList)
-
-        elif msgType == '/AlterPasswd':
-            username, data, nickname, win, lose, draw = self.factory.player[self.factory.client.index(self)]
-            update(username, msg, nickname, win, lose, draw)
-            self.factory.player[self.factory.client.index(self)] = (username, msg, nickname, win, lose, draw)
-            data = '/AlterPasswdSuccess ' + msg
-
-        elif msgType == '/Invite':
-            user = query2(msg)
-            if self.factory.status[self.factory.player.index(user[0])] == "/idle":
-                self.factory.client[self.factory.player.index(user[0])].transport.write(
-                    "/Invited " + str(self.factory.player[self.factory.client.index(self)][2]))
-                return
-            else:
-                data = '/InviteFailed 0'
-
-        elif msgType == '/InviteSuccess':
-            user = query2(msg)
-            self.factory.status[self.factory.player.index(user[0])] = self.factory.player[
-                self.factory.client.index(self)]
-            self.factory.status[self.factory.client.index(self)] = user[0]
-            for i in range(225):
-                self.factory.chessBoard[self.factory.player.index(user[0])][i] = 255
-                self.factory.chessBoard[self.factory.client.index(self)][i] = 255
-            self.factory.client[self.factory.player.index(user[0])].transport.write("/InviteSuccess 1")
-            return
-
-        elif msgType == '/InviteFailed':
-            user = query2(msg)
-            self.factory.client[self.factory.player.index(user[0])].transport.write("/InviteFailed 1")
-            return
-
-        elif msgType == '/BeginWithAI':
-            for i in range(225):
-                self.factory.chessBoard[self.factory.client.index(self)][i] = 255
-            self.factory.chessBoard[self.factory.client.index(self)][112] = 0
-            self.factory.status[self.factory.client.index(self)] = "/AI"
-            data = "/NextStep 7 7"
-
-        elif msgType == '/NextStep':
-            x, y, chessType = msg.split(' ')
-            self.factory.chessBoard[self.factory.client.index(self)][int(y) * 15 + int(x)] = int(chessType)
-            user = self.factory.status[self.factory.client.index(self)]
-            if rule.GetIsWin(self.factory.chessBoard[self.factory.client.index(self)], int(x), int(y)):
-                data = "/Win"
-                if user != "/AI":
-                    self.factory.client[self.factory.player.index(user)].transport.write(
-                        "/Lose " + str(x) + " " + str(y))
-                    self.factory.status[self.factory.player.index(user)] = "/idle"
-                self.factory.status[self.factory.client.index(self)] = "/idle"
-            elif user == "/AI":
-                nextStep = ai.GetAGoodMove(self.factory.chessBoard[self.factory.client.index(self)], 3, 4)
-                a = nextStep / 100
-                b = nextStep - 100 * a
-                self.factory.chessBoard[self.factory.client.index(self)][b * 15 + a] = 0
-                if rule.GetIsWin(self.factory.chessBoard[self.factory.client.index(self)], a, b):
-                    data = "/Lose " + str(a) + " " + str(b)
-                    self.factory.status[self.factory.client.index(self)] = "/idle"
-                else:
-                    data = "/NextStep " + str(a) + " " + str(b)
-            else:
-                self.factory.chessBoard[self.factory.player.index(user)][int(y) * 15 + int(x)] = int(chessType)
-                self.factory.client[self.factory.player.index(user)].transport.write(
-                    "/NextStep " + str(x) + " " + str(y))
-                return
-
-        elif msgType == '/DrawChess':
-            user = self.factory.status[self.factory.client.index(self)]
-            if user == "/AI":
-                self.factory.status[self.factory.client.index(self)] = "/idle"
-                data = "/Draw"
-            else:
-                self.factory.client[self.factory.player.index(user)].transport.write("/IsDraw")
-                return
-
-        elif msgType == '/Draw':
-            user = self.factory.status[self.factory.client.index(self)]
-            self.factory.client[self.factory.player.index(user)].transport.write("/Draw")
-            self.factory.status[self.factory.player.index(user)] = "/idle"
-            self.factory.status[self.factory.client.index(self)] = "/idle"
-            return
-
-        elif msgType == '/NoDraw':
-            user = self.factory.status[self.factory.client.index(self)]
-            self.factory.client[self.factory.player.index(user)].transport.write("/NoDraw")
-            return
-
-        elif msgType == '/Surrender':
-            user = self.factory.status[self.factory.client.index(self)]
-            if user != "/AI":
-                self.factory.client[self.factory.player.index(user)].transport.write("/Win")
-                self.factory.status[self.factory.player.index(user)] = "/idle"
-            self.factory.status[self.factory.client.index(self)] = "/idle"
-            return
-
-        elif msgType == '/Logout':
-            del self.factory.player[self.factory.client.index(self)]
-            del self.factory.status[self.factory.client.index(self)]
-            del self.factory.chessBoard[self.factory.client.index(self)]
-            self.factory.client.remove(self)
-            userList = "/UpdateUserList "
-            for i in self.factory.player:
-                userList += str(i[2]) + ' '
-            userList = userList[:-1]
-            for client in self.factory.client:
-                client.transport.write(userList)
-            return
-
-        self.transport.write(data)
+        self.factory.handler[msg_type](self, msg)
 
 
 class ChessFactory(Factory):
     protocol = ChessServer
 
     def __init__(self):
-        self.client = []
-        self.player = []
-        self.status = []
-        self.chessBoard = []
+        self.handler = {
+            "/Login": self.login,
+            "/Register": self.register,
+            "/GetUserList": self.get_user_list,
+            "/GetUserInfo": self.get_user_info,
+            "/SendMsg": self.send_msg,
+            "/AlterName": self.alter_name,
+            "/AlterPassword": self.alter_password,
+            "/Invite": self.invite,
+            "/InviteSuccess": self.invite_success,
+            "/InviteFailed": self.invite_failed,
+            "/BeginWithAI": self.begin_with_ai,
+            "/NextStep": self.next_step,
+            "/DrawChess": self.draw_chess,
+            "/Draw": self.draw,
+            "/NoDraw": self.no_draw,
+            "/Surrender": self.surrender,
+            "/Logout": self.logout
+        }
+
+        self.client = {}
+        self.nickname = {}
+        self.username = {}
+
+    def send_to_client(self, client, msg):
+        client.transport.write(msg)
+
+    def send_to_all(self, msg):
+        for client in self.client.keys():
+            self.send_to_client(client, msg)
+
+    def update_user_list(self):
+        user_list = "/UpdateUserList "
+        for nickname in self.nickname.keys():
+            user_list += nickname + ' '
+
+        return user_list[:-1]
+
+    def add_user(self, client, user):
+        msg = str(user[0]) + ' ' + str(user[1]) + ' ' + str(user[2]) + ' ' + str(user[3]) + ' ' + str(
+            user[4]) + ' ' + str(user[5])
+        self.send_to_client(client, msg)
+
+        self.nickname[str(user[2])] = str(user[0])
+        self.username[str(user[0])] = {
+            "client": client,
+            "nickname": str(user[2]),
+            "password": str(user[1]),
+            "win": int(user[3]),
+            "lose": int(user[4]),
+            "draw": int(user[5]),
+            "status": "/idle",
+            "level": 0,
+            "chessBoard": chessBoard()
+        }
+
+        user_list = self.update_user_list()
+        self.send_to_all(user_list)
+
+        self.client[client] = str(user[0])
+
+    def get_user(self, client, nickname):
+        if client is not None:
+            return self.username[self.client[client]]
+
+        elif nickname is not None:
+            return self.username[self.nickname[nickname]]
+
+        else:
+            return None
+
+    def login(self, client, msg):
+        username, password = msg.split(' ')
+        user = query(username, None)
+
+        if user == ():
+            self.send_to_client(client, "/UsernameNotExist")
+
+        elif user[0][1] != password:
+            self.send_to_client(client, "/PasswordError")
+
+        elif user[0][0] in self.username.keys():
+            self.send_to_client(client, "/UserHasLogged")
+
+        else:
+            self.add_user(client, user[0])
+
+    def register(self, client, msg):
+        username, password = msg.split(' ')
+        user = query(username, None)
+
+        if user:
+            self.send_to_client(client, "/UsernameExist")
+
+        else:
+            insert(username, password, username)
+            user = query(username, None)
+
+            self.add_user(client, user[0])
+
+    def get_user_list(self, client, msg):
+        self.send_to_client(client, self.update_user_list())
+
+    def get_user_info(self, client, msg):
+        user = self.get_user(None, msg)
+        msg = "/UserInfo " + user["nickname"] + ' ' + str(user["win"]) + ' ' + str(user["lose"]) + ' ' + str(
+            user["draw"])
+
+        self.send_to_client(client, msg)
+
+    def send_msg(self, client, msg):
+        self.send_to_all("/SendMsg " + msg)
+
+    def alter_name(self, client, msg):
+        user = query(None, msg)
+
+        if user:
+            self.send_to_client(client, "/AlterNameError")
+
+        else:
+            username = self.client[client]
+            nickname = self.username[username]["nickname"]
+            update(username, None, msg, None, None, None)
+
+            del self.nickname[nickname]
+            self.nickname[msg] = username
+            self.username[username]["nickname"] = msg
+
+            self.send_to_client(client, "/AlterNameSuccess " + msg)
+
+            user_list = self.update_user_list()
+            for key in self.client.keys():
+                if key != client:
+                    self.send_to_client(key, user_list)
+
+    def alter_password(self, client, msg):
+        username = self.client[client]
+        update(username, msg, None, None, None, None)
+
+        self.username[username]["password"] = msg
+
+        self.send_to_client(client, "/AlterPasswordSuccess " + msg)
+
+    def invite(self, client, msg):
+        user = self.get_user(None, msg)
+
+        if user["status"] == "/idle":
+            self.send_to_client(user["client"], "/Invited " + self.get_user(client, None)["nickname"])
+
+        else:
+            self.send_to_client(client, "/InviteFailed 0")
+
+    def invite_success(self, client, msg):
+        user = self.get_user(client, None)
+        user2 = self.get_user(None, msg)
+
+        user["status"] = user2["nickname"]
+        user2["status"] = user["nickname"]
+
+        for i in range(225):
+            user["chessBoard"][i] = 255
+            user2["chessBoard"][i] = 255
+
+        self.send_to_client(user2["client"], "/InviteSuccess 1")
+
+    def invite_failed(self, client, msg):
+        user = self.get_user(None, msg)
+
+        self.send_to_client(user["client"], "/InviteFailed 1")
+
+    def begin_with_ai(self, client, msg):
+        user = self.get_user(client, None)
+
+        for i in range(225):
+            user["chessBoard"][i] = 255
+        user["chessBoard"][112] = 0
+        user["status"] = "/AI"
+
+        self.send_to_client(client, "/NextStep 7 7")
+
+    def next_step(self, client, msg):
+        x, y, chessType = msg.split(' ')
+        user = self.get_user(client, None)
+        user2 = self.get_user(None, user["status"])
+
+        user["chessBoard"][int(y) * 15 + int(x)] = int(chessType)
+
+        if rule.GetIsWin(user["chessBoard"], int(x), int(y)):
+            self.send_to_client(client, "/Win")
+
+            if user["status"] != "/AI":
+                self.send_to_client(user2["client"], "/Lose " + str(x) + " " + str(y))
+                user2["status"] = "/idle"
+
+            user["status"] = "/idle"
+
+        elif user["status"] == "/AI":
+            nextStep = ai.GetAGoodMove(user["chessBoard"], 3)
+            a = nextStep / 100
+            b = nextStep - 100 * a
+
+            user["chessBoard"][b * 15 + a] = 0
+
+            if rule.GetIsWin(user["chessBoard"], a, b):
+                self.send_to_client(client, "/Lose " + str(a) + " " + str(b))
+                user["status"] = "/idle"
+
+            else:
+                self.send_to_client(client, "/NextStep " + str(a) + " " + str(b))
+
+        else:
+            user2["chessBoard"][int(y) * 15 + int(x)] = int(chessType)
+            self.send_to_client(user2["client"], "/NextStep " + str(x) + " " + str(y))
+
+    def draw_chess(self, client, msg):
+        user = self.get_user(client, None)
+
+        if user["status"] == "/AI":
+            user["status"] = "/idle"
+
+            self.send_to_client(client, "/Draw")
+
+        else:
+            self.send_to_client(self.get_user(None, user["status"])["client"], "/IsDraw")
+
+    def draw(self, client, msg):
+        user = self.get_user(client, None)
+        user2 = self.get_user(None, user["status"])
+
+        self.send_to_client(user2["client"], "/Draw")
+
+        user["status"] = "/idle"
+        user2["status"] = "/idle"
+
+    def no_draw(self, client, msg):
+        user = self.get_user(None, self.get_user(client, None)["status"])
+
+        self.send_to_client(user["client"], "/NoDraw")
+
+    def surrender(self, client, msg):
+        user = self.get_user(client, None)
+        user2 = self.get_user(None, user["status"])
+
+        if user["status"] != "/AI":
+            self.send_to_client(user2["client"], "/Win")
+            user2["status"] = "/idle"
+
+        user["status"] = "/idle"
+
+    def logout(self, client, msg):
+        username = self.client[client]
+
+        del self.client[client]
+        del self.nickname[self.username[username]["nickname"]]
+        del self.username[username]
+
+        user_list = self.update_user_list()
+        self.send_to_all(user_list)
 
 
 reactor.listenTCP(7110, ChessFactory())
 reactor.run()
-
-
