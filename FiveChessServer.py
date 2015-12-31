286c406041cc2f80ac7d2c17c72d1f2018f1498d
Fate Solo <FateSolo@mail.bistu.edu.cn>
2015-12-31 18:20:23 +0800

    类型转换

--- FiveChessServer.py
+++ FiveChessServer.py
@@ -260,7 +260,7 @@ class ChessFactory(ServerFactory):
                 if next_step == -1:
                     next_step = ai.GetAGoodMove(user["chessBoard"], user["level"])
 
-                a = next_step / 100
+                a = int(next_step / 100)
                 b = next_step - 100 * a
                 user["chessBoard"][b * 15 + a] = 0
 
